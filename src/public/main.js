bc2935c8b4c58fd60c9426bb00d5654939138800
Chris <cpiccini11@gmail.com>
2023-12-07 10:44:38 -0500

    Converted app from directory upload to file upload with ability to select multiple files.  This is a cleaner approach for end users.

--- src/public/main.js
+++ src/public/main.js
@@ -17,7 +17,7 @@
     along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 */
-const folderInput = document.getElementById("directory-input")
+const folderInput = document.getElementById("files-input")
 const compressImagesBtn = document.getElementById("compress-images")
 
 folderInput.addEventListener("change", e => {
